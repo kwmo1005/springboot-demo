51734dd14638fa6b091f46ed38e8524903debc5f
kwmo1005 <kwmo1005@163.com>
2020-02-13 15:17:48 +0800

    v1

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -21,7 +21,7 @@ public class StudentController {
 
     @RequestMapping(value = "/qryById")
     public Student qry(int id){
-        System.out.println("修改123456");
+        System.out.println("this is v1");
         return service.queryById(id);
     }
 }
